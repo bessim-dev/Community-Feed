498fe7314507de44c17467de55224bf3cf3d6003
becem-dev <you@example.com>
2020-09-27 11:34:47 +0100

    setup react router and create Question component

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Card from '../components/Card/Card';
-
+import React, { Component } from "react";
+import styled from "styled-components";
+import Card from "../components/Card/Card";
+import { Link } from "react-router-dom";
 const FeedWrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -12,23 +12,23 @@ const FeedWrapper = styled.div`
 const Alert = styled.div`
   text-align: center;
 `;
+const CardLink =styled(Link)`
+text-decoration : none;
+color : inherit;
+`
 
-const ROOT_API = 'https://api.stackexchange.com/2.2/';
+const ROOT_API = "https://api.stackexchange.com/2.2/";
 
 class Feed extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-      loading: true,
-      error: '',
-    };
-  }
-
+  state = {
+    data: [],
+    loading: true,
+    error: "",
+  };
   async componentDidMount() {
     try {
       const data = await fetch(
-        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`,
+        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`
       );
       const dataJSON = await data.json();
 
@@ -50,13 +50,15 @@ class Feed extends Component {
     const { data, loading, error } = this.state;
 
     if (loading || error) {
-      return <Alert>{loading ? 'Loading...' : error}</Alert>;
+      return <Alert>{loading ? "Loading..." : error}</Alert>;
     }
 
     return (
       <FeedWrapper>
-        {data.items.map(item => (
-          <Card key={item.question_id} data={item} />
+        {data.items.map((item) => (
+          <CardLink key={item.question_id} to={`questions/${item.question_id}`}>
+            <Card data={item} />
+          </CardLink>
         ))}
       </FeedWrapper>
     );
